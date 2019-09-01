4432e02ba4e9cf0f720ef70143b3705d7f70fbd0
Justin <justin@phazon.app>
2019-09-01 15:32:03 -0500

    Changed return object in newDBobject

--- multisql.go
+++ multisql.go
@@ -9,14 +9,9 @@ import (
 	"github.com/phazon85/multisql/config"
 )
 
-//SQLService holds connection blob from NewDBObject
-type SQLService struct {
-	DB *sql.DB
-}
-
 //NewDBObject takes in file name and driver name and return an appropriate DB connection object
 // depending on which driver name is specified - Currently supports Postgresql only
-func NewDBObject(file string, driverName string) *SQLService {
+func NewDBObject(file string, driverName string) *sql.DB {
 	cfg, err := config.NewConfig(driverName, file)
 	if err != nil {
 		log.Printf("Error crearing connection string, %s", err)
@@ -30,7 +25,5 @@ func NewDBObject(file string, driverName string) *SQLService {
 		log.Printf("Error pinging SQL DB: %s", err.Error())
 	}
 	log.Println("Succesfully connected.")
-	return &SQLService{
-		DB: db,
-	}
+	return db
 }
